05b7135540ec1353037a2cb3f5d4f3983b3343c4
Andrew Berry <Andrew@6162s.net>
2021-08-31 22:18:24 +1000

    Wrote version 1

---
+++ generate-spreadsheet.py
@@ -0,0 +1,55 @@
+from openpyxl import load_workbook
+from datetime import datetime
+import csv
+now = datetime.now() # current date and time
+d = now.strftime("%d-%m-%Y")
+colums="ABCDEFGHIJLMNOPQRSTUVWXYZ"
+colums=list(colums)
+# Start by opening the spreadsheet and selecting the main sheet
+workbook = load_workbook(filename="reports/Visitor-Template.xlsx")
+sheet = workbook.active
+with open('visitors.csv') as csvDataFile:
+	csvReader = csv.reader(csvDataFile)
+	rowcount=3
+	colnum=0
+	for row in csvReader:
+		print(row)
+		if row[0]==d:
+			sheet[str(colums[colnum]+str(rowcount))] = row[2]
+			sheet[str(colums[(colnum+1)]+str(rowcount))] = row[1]
+			sheet[str(colums[(colnum+2)]+str(rowcount))] = row[3]
+			sheet[str(colums[(colnum+3)]+str(rowcount))] = row[4]
+			sheet[str(colums[(colnum+4)]+str(rowcount))] = row[5]
+			rowcount+=1
+			if rowcount > 38:
+				rowcount=3
+				colnum+=5
+# Write what you want into a specific cell
+
+# Save the spreadsheet
+workbook.save(filename="reports/Visitor_Log-%s.xlsx" % d)
+
+
+
+# workbook = load_workbook(filename="reports/Report-Template.xlsx")
+# sheet = workbook.active
+# with open('visitors.csv') as csvDataFile:
+	# csvReader = csv.reader(csvDataFile)
+	# rowcount=3
+	# colnum=0
+	# for row in csvReader:
+		# print(row)
+		# if row[0]==d:
+			# sheet[str(colums[colnum]+str(rowcount))] = row[2]
+			# sheet[str(colums[(colnum+1)]+str(rowcount))] = row[1]
+			# sheet[str(colums[(colnum+2)]+str(rowcount))] = row[3]
+			# sheet[str(colums[(colnum+3)]+str(rowcount))] = row[4]
+			# sheet[str(colums[(colnum+4)]+str(rowcount))] = row[5]
+			# rowcount+=1
+			# if rowcount > 38:
+				# rowcount=3
+				# colnum+=5
+# # Write what you want into a specific cell
+
+# # Save the spreadsheet
+# workbook.save(filename="reports/Visitor_Log-%s.xlsx" % d)
---
+++ main.go
@@ -0,0 +1,149 @@
+// forms.go
+package main
+
+import (
+    "html/template"
+    "net/http"
+    "fmt"
+    "encoding/csv"
+    "os"
+    "time"
+    "os/exec"
+)
+
+type ContactDetails struct {
+    plate	string
+    vehicle_type	string
+    waste_type	string
+    commercial	string
+}
+type Daily struct {
+    rainfall	string
+    waste_bins	string
+    comingled_bins	string
+    cardboard_bins	string
+    comments	string
+}
+type Weekly struct {
+    scrap_wetal	string
+    green_waste	string
+    batteries	string
+    oil	string
+    generator_fuel string
+    generator_started string
+}
+
+type Monthly struct{
+	fire_extinguisher string
+	first_aid	string
+	septic_level string
+	water_tank string
+}
+
+
+
+func main() {
+	http.HandleFunc("/daily/", daily)
+	http.HandleFunc("/new_visitor/", new_visitor)
+	http.HandleFunc("/generate", generate)
+	http.Handle("/reports/", http.StripPrefix("/reports/", http.FileServer(http.Dir("./reports"))))
+	http.HandleFunc("/", main_page)
+	fmt.Println("Listening and Serving")
+    http.ListenAndServe(":8080", nil)
+    
+}
+
+func main_page(w http.ResponseWriter, r *http.Request){
+	
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	if r.Method != http.MethodPost {
+            tmpl.Execute(w, nil)
+            return
+        }
+	}
+
+func generate(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Generating reports")
+	cmd := exec.Command("generate.bat")
+out, err := cmd.Output()
+
+if err != nil {
+    println(err.Error())
+    return
+}
+
+fmt.Println(string(out))
+	exec.Command("C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python38\\python.exe generate-spreadsheet.py").Run()
+	tmpl := template.Must(template.ParseFiles("generate.html"))
+	
+	if r.Method != http.MethodPost {
+            tmpl.Execute(w, nil)
+            return
+        }
+	}
+
+
+func daily(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("daily.html"))
+	if r.Method != http.MethodPost {
+            tmpl.Execute(w, nil)
+            return
+        }
+
+        details := Daily{
+            rainfall:   r.FormValue("rainfall"),
+            waste_bins: r.FormValue("waste_bins"),
+            comingled_bins: r.FormValue("comingled_bins"),
+            cardboard_bins: r.FormValue("cardboard_bins"),
+            comments: r.FormValue("comments"),
+        }
+
+        // do something with details
+        fmt.Println(details)
+        var f, err = os.OpenFile("daily.csv", os.O_APPEND|os.O_CREATE, 0755)
+        if err!=nil{
+			fmt.Println(err)
+			}
+		writer := csv.NewWriter(f)
+		dt:=time.Now()
+		var data = [][]string{
+        {dt.Format("02-01-2006"), details.rainfall, details.waste_bins, details.comingled_bins, details.cardboard_bins, details.comments},
+    }
+    writer.WriteAll(data)
+    f.Close()
+        
+        tmpl.Execute(w, struct{ Success bool }{true})
+    }
+
+
+
+func new_visitor(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("visitor.html"))
+	if r.Method != http.MethodPost {
+            tmpl.Execute(w, nil)
+            return
+        }
+
+        details := ContactDetails{
+            plate:   r.FormValue("plate"),
+            vehicle_type: r.FormValue("vehicle_type"),
+            waste_type: r.FormValue("waste_type"),
+            commercial: r.FormValue("commercial"),
+        }
+
+        // do something with details
+        fmt.Println(details)
+        var f, err = os.OpenFile("visitors.csv", os.O_APPEND|os.O_CREATE, 0755)
+        if err!=nil{
+			fmt.Println(err)
+			}
+		writer := csv.NewWriter(f)
+		dt:=time.Now()
+		var data = [][]string{
+        {dt.Format("02-01-2006"), dt.Format("15:04:05"), details.plate, details.vehicle_type, details.waste_type, details.commercial},
+    }
+    writer.WriteAll(data)
+    f.Close()
+        
+        tmpl.Execute(w, struct{ Success bool }{true})
+    }
