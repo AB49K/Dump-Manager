dd05b93045caf37374de7fb6623c87f1f0987cf9
Andrew Berry <andrew.berry@comms.6162s.net>
2021-11-04 15:15:21 +1000

    Updated generate-spreadsheet.py to conform with EPA requirements

--- generate-spreadsheet.py
+++ generate-spreadsheet.py
@@ -6,7 +6,11 @@ import sys
 print("Generating spreadsheets")
 now = datetime.now() # current date and time
 #d = now.strftime("%d-%m-%Y")
+
+#Get the date from system args, if none are supplied, use current date
 d=sys.argv[1]
+if d==None:
+    d==now
 colums="ABCDEFGHIJLMNOPQRSTUVWXYZ"
 colums=list(colums)
 # Start by opening the spreadsheet and selecting the main sheet
@@ -18,42 +22,46 @@ with open('visitors.csv') as csvDataFile:
 	colnum=0
 	for row in csvReader:
 		print(row)
+                #These values are all required by EPA rules.
+                #VTC[0]=="Car"
+                #VTC[1]=="Car + Trailer"
+                #VTC[2]=="Ute"
+                #VTC[3]=="Ute + Trailer"
+                #VTC[4]=="GVM (Truck)"
+                #VTC[5]=="Van"
+                VTC=["Car","Car + Trailer", "Ute", "Ute + Trailer", "GVM", "Van"]
+
+                #This is just a list to keep count of the numbers of each type of visitor
+
+                VTC_INT=[0,0,0,0,0,0]
+                #I am well aware of how wasteful and slow this method is, and just how much better using a database would be.
+                #However due to the formats that my employer wants this data, spreadsheets unfortunately are the way to go.
 		if row[0]==d:
+                        #Increment the vehicle type by one.
+                        VTC_INT[int(row[3])]+=1
+
+
+                        #Time
 			sheet[str(colums[colnum]+str(rowcount))] = row[2]
-			sheet[str(colums[(colnum+1)]+str(rowcount))] = row[1]
-			sheet[str(colums[(colnum+2)]+str(rowcount))] = row[3]
-			sheet[str(colums[(colnum+3)]+str(rowcount))] = row[4]
-			sheet[str(colums[(colnum+4)]+str(rowcount))] = row[5]
+			#Registration
+                        sheet[str(colums[(colnum+1)]+str(rowcount))] = row[1]
+			#Vehicle Type
+                        sheet[str(colums[(colnum+2)]+str(rowcount))] = VCT[int(row[3])]
+			#Waste Type
+                        sheet[str(colums[(colnum+3)]+str(rowcount))] = row[4]
+			#Commercial or Domestic
+                        sheet[str(colums[(colnum+4)]+str(rowcount))] = row[5]
 			rowcount+=1
 			if rowcount > 38:
 				rowcount=3
 				colnum+=5
-# Write what you want into a specific cell
 
-# Save the spreadsheet
+#Writing down the vehicle type counter.                                
+sheet['A40']=str(VTC_INT[0])
+sheet['B40']=str(VTC_INT[1])
+sheet['C40']=str(VTC_INT[2])
+sheet['D40']=str(VTC_INT[3])
+sheet['E40']=str(VTC_INT[4])
+sheet['F40']=str(VTC_INT[5])
 workbook.save(filename="reports/Visitor_Log-%s.xlsx" % d)
 
-
-
-# workbook = load_workbook(filename="reports/Report-Template.xlsx")
-# sheet = workbook.active
-# with open('visitors.csv') as csvDataFile:
-	# csvReader = csv.reader(csvDataFile)
-	# rowcount=3
-	# colnum=0
-	# for row in csvReader:
-		# print(row)
-		# if row[0]==d:
-			# sheet[str(colums[colnum]+str(rowcount))] = row[2]
-			# sheet[str(colums[(colnum+1)]+str(rowcount))] = row[1]
-			# sheet[str(colums[(colnum+2)]+str(rowcount))] = row[3]
-			# sheet[str(colums[(colnum+3)]+str(rowcount))] = row[4]
-			# sheet[str(colums[(colnum+4)]+str(rowcount))] = row[5]
-			# rowcount+=1
-			# if rowcount > 38:
-				# rowcount=3
-				# colnum+=5
-# # Write what you want into a specific cell
-
-# # Save the spreadsheet
-# workbook.save(filename="reports/Visitor_Log-%s.xlsx" % d)
